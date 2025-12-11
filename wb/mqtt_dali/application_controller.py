e7d323947ec252133cdeb8820315647ae5296e07
Nikolay Korotkiy <sikmir@disroot.org>
2025-12-12 03:20:31 +0400

    Enable lunatone gw

--- wb/mqtt_dali/application_controller.py
+++ wb/mqtt_dali/application_controller.py
@@ -6,6 +6,7 @@ from dali.device.general import StartQuiescentMode, StopQuiescentMode
 
 from .commissioning import Commissioning
 from .dali_device import DaliDevice, make_device
+from .fake_lunatone_iot import run_websocket
 from .mqtt_dispatcher import MQTTDispatcher
 from .wbdali import AsyncDeviceInstanceTypeMapper, WBDALIConfig, WBDALIDriver
 
@@ -17,6 +18,8 @@ class ApplicationController:
         bus_index: int,
         devices: list[DaliDevice],
         mqtt_dispatcher: MQTTDispatcher,
+        websocket_enabled: bool = False,
+        websocket_port: int = 8080,
     ) -> None:
         self.uid = f"{mqtt_device_id}_{bus_index}"
         self.bus_name = f"Bus {bus_index}"
@@ -30,11 +33,28 @@ class ApplicationController:
         )
         self.dev = WBDALIDriver(cfg, mqtt_dispatcher=self.mqtt_dispatcher, dev_inst_map=self.dev_inst_map)
         self._commissioning_task: Optional[asyncio.Task] = None
+        self._websocket_enabled = websocket_enabled
+        self._websocket_port = websocket_port
+        self._websocket_task: Optional[asyncio.Task] = None
 
     async def start(self) -> None:
         await self.dev.initialize()
 
+        if self._websocket_enabled:
+            self._websocket_task = asyncio.create_task(
+                self._run_websocket(),
+                name=f"websocket-{self.uid}",
+            )
+
     async def stop(self) -> None:
+        if self._websocket_task:
+            self._websocket_task.cancel()
+            try:
+                await self._websocket_task
+            except asyncio.CancelledError:
+                pass
+            self._websocket_task = None
+
         await self.dev.deinitialize()
         if self._commissioning_task:
             self._commissioning_task.cancel()
@@ -65,3 +85,14 @@ class ApplicationController:
         new_devices = [make_device(self.uid, addr) for addr in res.new]
         self.devices = unchanged_devices + changed_devices + new_devices
         self.devices.sort(key=lambda d: d.address.short)
+
+    async def _run_websocket(self) -> None:
+        try:
+            await run_websocket(
+                self.dev,
+                asyncio,
+                "0.0.0.0",
+                self._websocket_port,
+            )
+        except asyncio.CancelledError:
+            raise

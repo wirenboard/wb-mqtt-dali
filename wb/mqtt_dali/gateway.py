753ea21f539e7b295ca3972e3117f0616999d851
Nikolay Korotkiy <sikmir@disroot.org>
2026-01-26 14:47:52 +0400

    fixup

--- wb/mqtt_dali/gateway.py
+++ wb/mqtt_dali/gateway.py
@@ -340,7 +340,7 @@ class Gateway:
                 if device.get("device_type") == "WB-MDALI":
                     device_id = device.get("id")
                     if device_id is None:
-                        device_id = f"wb-mdali_{device.get('slave_id', '')}"
+                        device_id = f"wb-dali_{device.get('slave_id', '')}"
                     device_ids.add(device_id)
         async with self._config_lock:
             new_gateways = []

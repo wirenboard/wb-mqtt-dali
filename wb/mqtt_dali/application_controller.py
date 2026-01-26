3cd09a40dc4cf0cc4e56977911eae81bd8499ab3
Nikolay Korotkiy <sikmir@disroot.org>
2026-01-26 11:03:34 +0400

    fixup

--- wb/mqtt_dali/application_controller.py
+++ wb/mqtt_dali/application_controller.py
@@ -72,10 +72,8 @@ class ApplicationController:
 
         self._dev_inst_map = AsyncDeviceInstanceTypeMapper()
         cfg = WBDALIConfig(
-            channel=config.bus_index + 1,
-            modbus_port_path="/dev/ttyRS485-1",
             device_name=config.mqtt_device_id,
-            modbus_slave_id=1,
+            channel=config.bus_index + 1,
         )
 
         self._polling_interval = config.polling_interval

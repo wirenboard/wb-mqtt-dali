66dbd5362c9d41d0fd7d8e0a7e31fed4d28743a8
Nikolay Korotkiy <sikmir@disroot.org>
2026-02-06 14:55:07 +0400

    fixup

--- wb/mqtt_dali/dali2_type1_parameters.py
+++ wb/mqtt_dali/dali2_type1_parameters.py
@@ -1,10 +1,7 @@
 # Type 1 push buttons
 
-from typing import Callable, List
+from typing import List
 
-from dali.address import DeviceShort, InstanceNumber
-from dali.command import Command
-from dali.device.general import DTR0
 from dali.device.pushbutton import (
     QueryDoubleTimer,
     QueryRepeatTimer,
@@ -16,64 +13,30 @@ from dali.device.pushbutton import (
     SetStuckTimer,
 )
 
-from .settings import (
-    InstanceAddress,
-    NumberSettingsParam,
-    SettingsParamAddress,
-    SettingsParamName,
-)
+from .dali_2_device import InstanceParam
 
 
-class _PushButtonParam(NumberSettingsParam):
-    def __init__(
-        self,
-        name: str,
-        property_name: str,
-        query_command: Callable[[DeviceShort, InstanceNumber], Command],
-        set_command: Callable[[DeviceShort, InstanceNumber], Command],
-    ) -> None:
-        super().__init__(SettingsParamName(name), property_name)
-        self._query_command = query_command
-        self._set_command = set_command
-
-    def _ensure_instance_address(self, address: SettingsParamAddress) -> InstanceAddress:
-        if not isinstance(address, InstanceAddress):
-            raise ValueError("Address must be an InstanceAddress")
-        return address
-
-    def get_read_command(self, address: SettingsParamAddress) -> Command:
-        instance_address = self._ensure_instance_address(address)
-        return self._query_command(instance_address.device_short, instance_address.instance_number)
-
-    def get_write_commands(self, address: SettingsParamAddress, value_to_set: int) -> list[Command]:
-        instance_address = self._ensure_instance_address(address)
-        return [
-            DTR0(value_to_set),
-            self._set_command(instance_address.device_short, instance_address.instance_number),
-        ]
-
-
-class DoubleTimerParam(_PushButtonParam):
+class DoubleTimerParam(InstanceParam):
     def __init__(self) -> None:
         super().__init__("Double timer", "double_timer", QueryDoubleTimer, SetDoubleTimer)
 
 
-class ReportTimerParam(_PushButtonParam):
+class ReportTimerParam(InstanceParam):
     def __init__(self) -> None:
         super().__init__("Report timer", "report_timer", QueryRepeatTimer, SetRepeatTimer)
 
 
-class ShortTimerParam(_PushButtonParam):
+class ShortTimerParam(InstanceParam):
     def __init__(self) -> None:
         super().__init__("Short timer", "short_timer", QueryShortTimer, SetShortTimer)
 
 
-class StuckTimerParam(_PushButtonParam):
+class StuckTimerParam(InstanceParam):
     def __init__(self) -> None:
         super().__init__("Stuck timer", "stuck_timer", QueryStuckTimer, SetStuckTimer)
 
 
-def build_type1_push_button_parameters() -> List[NumberSettingsParam]:
+def build_type1_push_button_parameters() -> List[InstanceParam]:
     return [
         DoubleTimerParam(),
         ReportTimerParam(),
--- wb/mqtt_dali/dali2_type4_parameters.py
+++ wb/mqtt_dali/dali2_type4_parameters.py
@@ -1,10 +1,7 @@
 # Type 4 light sensor
 
-from typing import Callable, List
+from typing import List
 
-from dali.address import DeviceShort, InstanceNumber
-from dali.command import Command
-from dali.device.general import DTR0
 from dali.device.light import (
     QueryDeadtimeTimer,
     QueryHysteresis,
@@ -16,64 +13,30 @@ from dali.device.light import (
     SetReportTimer,
 )
 
-from .settings import (
-    InstanceAddress,
-    NumberSettingsParam,
-    SettingsParamAddress,
-    SettingsParamName,
-)
+from .dali_2_device import InstanceParam
 
 
-class _LightSensorParam(NumberSettingsParam):
-    def __init__(
-        self,
-        name: str,
-        property_name: str,
-        query_command: Callable[[DeviceShort, InstanceNumber], Command],
-        set_command: Callable[[DeviceShort, InstanceNumber], Command],
-    ) -> None:
-        super().__init__(SettingsParamName(name), property_name)
-        self._query_command = query_command
-        self._set_command = set_command
-
-    def _ensure_instance_address(self, address: SettingsParamAddress) -> InstanceAddress:
-        if not isinstance(address, InstanceAddress):
-            raise ValueError("Address must be an InstanceAddress")
-        return address
-
-    def get_read_command(self, address: SettingsParamAddress) -> Command:
-        instance_address = self._ensure_instance_address(address)
-        return self._query_command(instance_address.device_short, instance_address.instance_number)
-
-    def get_write_commands(self, address: SettingsParamAddress, value_to_set: int) -> list[Command]:
-        instance_address = self._ensure_instance_address(address)
-        return [
-            DTR0(value_to_set),
-            self._set_command(instance_address.device_short, instance_address.instance_number),
-        ]
-
-
-class DeadtimeTimerParam(_LightSensorParam):
+class DeadtimeTimerParam(InstanceParam):
     def __init__(self) -> None:
         super().__init__("Deadtime timer", "deadtime_timer", QueryDeadtimeTimer, SetDeadtimeTimer)
 
 
-class ReportTimerParam(_LightSensorParam):
+class ReportTimerParam(InstanceParam):
     def __init__(self) -> None:
         super().__init__("Report timer", "report_timer", QueryReportTimer, SetReportTimer)
 
 
-class HysteresisParam(_LightSensorParam):
+class HysteresisParam(InstanceParam):
     def __init__(self) -> None:
         super().__init__("Hysteresis", "hysteresis", QueryHysteresis, SetHysteresis)
 
 
-class HysteresisMinParam(_LightSensorParam):
+class HysteresisMinParam(InstanceParam):
     def __init__(self) -> None:
         super().__init__("Hysteresis minimum", "hysteresis_min", QueryHysteresisMin, SetHysteresisMin)
 
 
-def build_type4_light_sensor_parameters() -> List[NumberSettingsParam]:
+def build_type4_light_sensor_parameters() -> List[InstanceParam]:
     return [
         DeadtimeTimerParam(),
         ReportTimerParam(),

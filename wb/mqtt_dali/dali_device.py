3f73515496f453acc64efbcce4ddfc02efcfb766
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2026-04-01 09:35:23 +0000

    Add colour setup

--- wb/mqtt_dali/dali_device.py
+++ wb/mqtt_dali/dali_device.py
@@ -5,10 +5,12 @@ from typing import Optional
 from dali.address import Address, GearShort
 from dali.gear.general import (
     IdentifyDevice,
+    Initialise,
     QueryDeviceType,
     QueryNextDeviceType,
     RecallMaxLevel,
     RecallMinLevel,
+    Terminate,
 )
 from dali.sequences import sleep as seq_sleep
 
@@ -37,6 +39,7 @@ from .dali_type5_parameters import Type5Parameters
 from .dali_type6_parameters import Type6Parameters
 from .dali_type7_parameters import Type7Parameters
 from .dali_type8_parameters import ColourType, Type8Parameters
+from .dali_type8_rgbwaf import MAX_COLOUR_VALUE, set_rgb_commands_builder
 from .dali_type16_parameters import Type16Parameters
 from .dali_type17_parameters import Type17Parameters
 from .dali_type20_parameters import Type20Parameters
@@ -137,22 +140,16 @@ class DaliDevice(DaliDeviceBase):
                 v = await query_response(driver, self._compat.QueryVersionNumber(address))
             except Exception:
                 continue
-            if v.value == 1:
-                continue
-            use_identify_cmd = True
+            use_identify_cmd = v.value != 1
             break
         if use_identify_cmd:
             await driver.send(IdentifyDevice(address))
         else:
-            await driver.send(self._compat.Initialise(self.address.short))
-            try:
-                for _ in range(5):
-                    await driver.send(RecallMaxLevel(address))
-                    await asyncio.sleep(0.5)
-                    await driver.send(RecallMinLevel(address))
-                    await asyncio.sleep(0.5)
-            finally:
-                await driver.send(self._compat.Terminate())
+            setup_rgb = (
+                self._type8_handler is not None
+                and self._type8_handler.default_colour_type == ColourType.RGBWAF
+            )
+            await legacy_identify_sequence(driver, address, setup_rgb)
 
     async def load_info(self, driver: WBDALIDriver, force_reload: bool = False) -> None:
         await super().load_info(driver, force_reload)
@@ -259,3 +256,20 @@ class DaliDevice(DaliDeviceBase):
                     group_parameter_handlers.extend(self._type8_handler.get_group_parameters())
 
         return (parameter_handlers, mqtt_controls, group_parameter_handlers)
+
+
+async def legacy_identify_sequence(driver: WBDALIDriver, address: GearShort, setup_rgb: bool) -> None:
+
+    await driver.send(Initialise(address=address.address))
+    if setup_rgb:
+        await driver.send_commands(
+            set_rgb_commands_builder(address, MAX_COLOUR_VALUE, MAX_COLOUR_VALUE, MAX_COLOUR_VALUE)
+        )
+    try:
+        for _ in range(5):
+            await driver.send(RecallMaxLevel(address))
+            await asyncio.sleep(0.5)
+            await driver.send(RecallMinLevel(address))
+            await asyncio.sleep(0.5)
+    finally:
+        await driver.send(Terminate())

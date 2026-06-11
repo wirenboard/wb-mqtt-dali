662415093b53e1d1b39eebcaf2a39c97be9e4417
KraPete <petr.krasnoshchekov@wirenboard.com>
2026-06-11 16:30:03 +0500

    Fix formatting

--- tests/test_mqtt_dispatcher_retained.py
+++ tests/test_mqtt_dispatcher_retained.py
@@ -63,8 +63,9 @@ async def test_unsubscribe_clears_retained_cache():
 
     cb1 = MagicMock()
     await dispatcher.subscribe("topic/z", cb1)
-    dispatcher._dispatch_message(aiomqtt.Message("topic/z", b"r", 0, True, 0, None))  # pylint: disable=protected-access
-
+    dispatcher._dispatch_message( # pylint: disable=protected-access
+        aiomqtt.Message("topic/z", b"r", 0, True, 0, None)
+    )
     await dispatcher.unsubscribe("topic/z")
 
     cb2 = MagicMock()
